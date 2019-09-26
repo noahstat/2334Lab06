b4edbf2acb68e20ae565902718a66ef7507c1a08
Noah Stasyszen <stasyszen@gmail.com>
2019-09-26 13:47:59 -0500

    Filled out all methods

--- Triangle.java
+++ Triangle.java
@@ -6,6 +6,14 @@
  */
 public abstract class Triangle extends Polygon
 {
+	//has the first side length
+	private double s1;
+	
+	//has the second side length
+	private double s2;
+	
+	//has the third side length
+	private double s3;
     /**
      * The triangle constructor. Add the sides to the sideLengths arraylist.
      * 
@@ -16,6 +24,13 @@ public abstract class Triangle extends Polygon
      */
     public Triangle(String id, double s1, double s2, double s3)
     {
-    	// TODO: implement this.
+    	super(id);
+    	this.s1 = s1;
+		this.s2 = s2;
+		this.s3 = s3;
+        sideLengths.add(s1);
+        sideLengths.add(s2);
+        sideLengths.add(s3);
+        
     }
 }
