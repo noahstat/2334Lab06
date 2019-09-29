5ae9526e85512f5f092c8cf792dc274bbf9ba8e9
Noah Stasyszen <stasyszen@gmail.com>
2019-09-28 20:21:21 -0500

    removed integer division in toString

--- Shape.java
+++ Shape.java
@@ -154,7 +154,7 @@ public abstract class Shape implements Comparable<Shape>
 	public String toString()
 	{	
 		
-		return (this.getShapeType() + "\t ID = " + this.getId() + "\t area = " + (Math.round(this.getArea() * 100))/100 + 
-				"\tperimeter = " + (Math.round(this.getPerimeter() * 100))/100);
+		return (this.getShapeType() + "\t ID = " + this.getId() + "\t area = " + (Math.round(this.getArea() * 100.0))/100.0 + 
+				"\tperimeter = " + (Math.round(this.getPerimeter() * 100.0))/100.0);
 	}
 }
